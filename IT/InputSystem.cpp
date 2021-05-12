850f47e69649b11b20c259111a4d690af6aa682e
Plasmat1x <damaghack@gmail.com>
2021-05-12 09:16:49 +0700

    mod

---
+++ IT/InputSystem.cpp
@@ -0,0 +1,88 @@
+#include "InputSystem.h"
+#include "Coordinator.h"
+#include "COM_LIB.h"
+
+#include <iostream>
+
+extern ECS::Coordinator gCoordinator;
+
+void SYS::InputSystem::input(float dt)
+{
+    for (auto const& entity : mEntities)
+    {
+        auto& Input = gCoordinator.GetComponent<COM::Input>(entity);
+        auto& Velocity = gCoordinator.GetComponent<COM::RigidBody>(entity);
+        auto& States = gCoordinator.GetComponent<COM::States>(entity);
+
+        for (auto& key : Input.KeyList)
+        {
+
+            if (sf::Keyboard::isKeyPressed(key))
+            {
+                if (sf::Keyboard::isKeyPressed(key) && key == sf::Keyboard::W)
+                {
+                    States.states.at("run") = true;
+                    States.states.at("idle") = false;
+                    States.states.at("move_up") = true;
+                    break;
+                }
+                if (sf::Keyboard::isKeyPressed(key) && key == sf::Keyboard::A)
+                {
+                    States.states.at("run") = true;
+                    States.states.at("idle") = false;
+                    States.states.at("direction") = false;
+                    States.states.at("move_left") = true;
+                    break;
+                }
+                if (sf::Keyboard::isKeyPressed(key) && key == sf::Keyboard::S)
+                {
+                    States.states.at("run") = true;
+                    States.states.at("idle") = false;
+                    States.states.at("move_down") = true;
+                    break;
+                }
+                if (sf::Keyboard::isKeyPressed(key) && key == sf::Keyboard::D)
+                {
+                    States.states.at("run") = true;
+                    States.states.at("idle") = false;
+                    States.states.at("direction") = true;
+                    States.states.at("move_right") = true;
+                    break;
+                }
+            }   
+            else
+            {
+                States.states.at("run") = false;
+                States.states.at("idle") = true;
+                States.states.at("move_up") = false;
+                States.states.at("move_left") = false;
+                States.states.at("move_down") = false;
+                States.states.at("move_right") = false;
+            }
+        }
+    }
+}
+
+/*
+ 
+ if (sf::Keyboard::isKeyPressed(key) && key == sf::Keyboard::W)
+            {
+                if (Velocity.velocity.y > 0.0f) { Velocity.velocity.y = std::fmin(Velocity.velocity.y - Velocity._ACCELERATION * dt, 0.0f); }
+                else { Velocity.velocity.y = std::fmax(Velocity.velocity.y - Velocity._ACCELERATION * dt, -Velocity._MAXSPEED); }
+            }
+            else if (sf::Keyboard::isKeyPressed(key) && key == sf::Keyboard::A)
+            {
+                if (Velocity.velocity.x > 0.0f) { Velocity.velocity.x = std::fmin(Velocity.velocity.x - Velocity._ACCELERATION * dt, 0.0f); }
+                else { Velocity.velocity.x = std::fmax(Velocity.velocity.x - Velocity._ACCELERATION * dt, -Velocity._MAXSPEED); }
+            }
+            else if (sf::Keyboard::isKeyPressed(key) && key == sf::Keyboard::S)
+            {
+                if (Velocity.velocity.y < 0.0f) { Velocity.velocity.y = std::fmax(Velocity.velocity.y + Velocity._ACCELERATION * dt, 0.0f); }
+                else { Velocity.velocity.y = std::fmin(Velocity.velocity.y + Velocity._ACCELERATION * dt, Velocity._MAXSPEED); }
+            }
+            else if (sf::Keyboard::isKeyPressed(key) && key == sf::Keyboard::D)
+            {
+                if (Velocity.velocity.x < 0.0f) { Velocity.velocity.x = std::fmax(Velocity.velocity.x + Velocity._ACCELERATION * dt, 0.0f); }
+                else { Velocity.velocity.x = std::fmin(Velocity.velocity.x + Velocity._ACCELERATION * dt, Velocity._MAXSPEED); }
+            }
+*/
---
+++ IT/PhysicsSystem.cpp
@@ -0,0 +1,64 @@
+#include "PhysicsSystem.h"
+#include "Coordinator.h"
+
+#include "COM_LIB.h"
+
+extern ECS::Coordinator gCoordinator;
+
+void SYS::PhysicsSystem::update(float dt)
+{
+    for (auto const& entity : mEntities)
+    {
+        auto& Position = gCoordinator.GetComponent<COM::Transform>(entity);
+        auto& Velocity = gCoordinator.GetComponent<COM::RigidBody>(entity);
+        auto& Rect = gCoordinator.GetComponent<COM::Hitbox>(entity);
+        auto& States = gCoordinator.GetComponent<COM::States>(entity);
+
+        float mass = 3.0f;
+
+        if (Velocity.velocity.y > 0.0f)
+        {
+            Velocity.velocity.y = int(std::fmax(Velocity.velocity.y - std::abs(Velocity.velocity.y * dt * mass), 0.0f));
+        }
+        else if (Velocity.velocity.y < 0.0f)
+        {
+            Velocity.velocity.y = int(std::fmin(Velocity.velocity.y + std::abs(Velocity.velocity.y * dt * mass), 0.0f));
+        }
+
+        if (Velocity.velocity.x > 0.0f)
+        {
+            Velocity.velocity.x = int(std::fmax(Velocity.velocity.x - std::abs(Velocity.velocity.x * dt * mass), 0.0f));
+        }
+        else if (Velocity.velocity.x < 0.0f)
+        {
+            Velocity.velocity.x = int(std::fmin(Velocity.velocity.x + std::abs(Velocity.velocity.x * dt * mass), 0.0f));
+        }
+
+        if (States.states.at("move_up"))
+        {
+            if (Velocity.velocity.y > 0.0f) { Velocity.velocity.y = std::fmin(Velocity.velocity.y - Velocity._ACCELERATION * dt, 0.0f); }
+            else { Velocity.velocity.y = std::fmax(Velocity.velocity.y - Velocity._ACCELERATION * dt, -Velocity._MAXSPEED); }
+        }
+        else if (States.states.at("move_left"))
+        {
+            if (Velocity.velocity.x > 0.0f) { Velocity.velocity.x = std::fmin(Velocity.velocity.x - Velocity._ACCELERATION * dt, 0.0f); }
+            else { Velocity.velocity.x = std::fmax(Velocity.velocity.x - Velocity._ACCELERATION * dt, -Velocity._MAXSPEED); }
+        }
+        else if (States.states.at("move_down"))
+        {
+            if (Velocity.velocity.y < 0.0f) { Velocity.velocity.y = std::fmax(Velocity.velocity.y + Velocity._ACCELERATION * dt, 0.0f); }
+            else { Velocity.velocity.y = std::fmin(Velocity.velocity.y + Velocity._ACCELERATION * dt, Velocity._MAXSPEED); }
+        }
+        else if (States.states.at("move_right")) 
+        {
+            if (Velocity.velocity.x < 0.0f) { Velocity.velocity.x = std::fmax(Velocity.velocity.x + Velocity._ACCELERATION * dt, 0.0f); }
+            else { Velocity.velocity.x = std::fmin(Velocity.velocity.x + Velocity._ACCELERATION * dt, Velocity._MAXSPEED); }
+        }
+
+
+        Position.position.x += Velocity.velocity.x * dt;
+        Position.position.y += Velocity.velocity.y * dt;
+
+        Rect.update(Position.position.x, Position.position.y);
+    }
+}
